34cfe1c806a7b4e8d5595d32deca429fb65e981e
xushengfeng <xushengfeng_zg@163.com>
2026-07-03 22:40:37 +0800

    wayland 升级xkb库，修复方向键错误

--- src/wayland/test/keyboard.test.ts
+++ src/wayland/test/keyboard.test.ts
@@ -4,7 +4,7 @@ import { testRunnerApp } from "../../test_runner/test_runner";
 import { mapKeyCode } from "../../input_map/web2x";
 
 describe("keyboard app", () => {
-    it("run test/simple_app/dmabuf_one_frame", async () => {
+    it("run test/simple_app/dmabuf_one_frame", { timeout: 8000 }, async () => {
         const k = () => {
             const input = document.createElement("input");
             document.body.appendChild(input);
@@ -35,21 +35,28 @@ describe("keyboard app", () => {
                     const win = client.win(windowId);
                     if (win) {
                         win.point.sendPointerEvent("move", { x: 16, y: 16, button: 0 });
-                        setTimeout(() => {
-                            client.keyboard.sendKey(30, "pressed");
-                        }, 50);
-                        setTimeout(() => {
-                            client.keyboard.sendKey(30, "released");
-                        }, 50 * 2);
-                        setTimeout(() => {
-                            client.keyboard.sendKey(48, "pressed");
-                        }, 50 * 3);
-                        setTimeout(() => {
-                            client.keyboard.sendKey(48, "released");
-                        }, 50 * 4);
-                        setTimeout(() => {
-                            runner.kill();
-                        }, 50 * 5);
+                        const keys = [30, 48, 28, 57, 103, 108, 105, 106, 82];
+                        for (const [i, key] of keys.entries()) {
+                            setTimeout(
+                                () => {
+                                    client.keyboard.sendKey(key, "pressed");
+                                },
+                                50 * (i * 2 + 1),
+                            );
+                            setTimeout(
+                                () => {
+                                    client.keyboard.sendKey(key, "released");
+                                },
+                                50 * (i * 2 + 2),
+                            );
+                        }
+
+                        setTimeout(
+                            () => {
+                                runner.kill();
+                            },
+                            50 * (keys.length * 2 + 1),
+                        );
                     }
                 }, 200);
             });
@@ -64,5 +71,19 @@ describe("keyboard app", () => {
         expect(result[2].data).toBe(JSON.stringify({ key: "a", code: "KeyA", type: "keyup" }));
         expect(result[3].data).toBe(JSON.stringify({ key: "b", code: "KeyB", type: "keydown" }));
         expect(result[4].data).toBe(JSON.stringify({ key: "b", code: "KeyB", type: "keyup" }));
+        expect(result[5].data).toBe(JSON.stringify({ key: "Enter", code: "Enter", type: "keydown" }));
+        expect(result[6].data).toBe(JSON.stringify({ key: "Enter", code: "Enter", type: "keyup" }));
+        expect(result[7].data).toBe(JSON.stringify({ key: " ", code: "Space", type: "keydown" }));
+        expect(result[8].data).toBe(JSON.stringify({ key: " ", code: "Space", type: "keyup" }));
+        expect(result[9].data).toBe(JSON.stringify({ key: "ArrowUp", code: "ArrowUp", type: "keydown" }));
+        expect(result[10].data).toBe(JSON.stringify({ key: "ArrowUp", code: "ArrowUp", type: "keyup" }));
+        expect(result[11].data).toBe(JSON.stringify({ key: "ArrowDown", code: "ArrowDown", type: "keydown" }));
+        expect(result[12].data).toBe(JSON.stringify({ key: "ArrowDown", code: "ArrowDown", type: "keyup" }));
+        expect(result[13].data).toBe(JSON.stringify({ key: "ArrowLeft", code: "ArrowLeft", type: "keydown" }));
+        expect(result[14].data).toBe(JSON.stringify({ key: "ArrowLeft", code: "ArrowLeft", type: "keyup" }));
+        expect(result[15].data).toBe(JSON.stringify({ key: "ArrowRight", code: "ArrowRight", type: "keydown" }));
+        expect(result[16].data).toBe(JSON.stringify({ key: "ArrowRight", code: "ArrowRight", type: "keyup" }));
+        expect(result[17].data).toBe(JSON.stringify({ key: "0", code: "Numpad0", type: "keydown" }));
+        expect(result[18].data).toBe(JSON.stringify({ key: "0", code: "Numpad0", type: "keyup" }));
     });
 });
